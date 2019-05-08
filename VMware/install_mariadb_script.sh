be771a319dc59c4dc8123651e1238943258b1ec5
bhadrim <bhadrim@ca.ibm.com>
2019-05-08 10:33:27 -0400

    Update install_mariadb_script.sh

--- VMware/install_mariadb_script.sh
+++ VMware/install_mariadb_script.sh
@@ -11,21 +11,21 @@ retryInstall () {
     $command && break
     let n=n+1
     if [ $n -eq $max ]; then
-      echo "---Exceed maximal number of retries---"
+      echo "---Exceed maximal number of retries---" >> $LOGFILE
       exit 1
     fi
     sleep 15
    done
 }
 echo "---start installing mariaDB---" | tee -a $LOGFILE
-retryInstall "yum install -y mariadb mariadb-server"        >> $LOGFILE 2>&1 || { echo "---Failed to install MariaDB---" | tee -a $LOGFILE; exit 1; }
-systemctl start mariadb                                     >> $LOGFILE 2>&1 || { echo "---Failed to start MariaDB---" | tee -a $LOGFILE; exit 1; }
-systemctl enable mariadb                                    >> $LOGFILE 2>&1 || { echo "---Failed to enable MariaDB---" | tee -a $LOGFILE; exit 1; }
+retryInstall "yum install -y mariadb mariadb-server"        >> $LOGFILE || { echo "---Failed to install MariaDB---" | tee -a $LOGFILE; exit 1; }
+systemctl start mariadb                                     >> $LOGFILE || { echo "---Failed to start MariaDB---" | tee -a $LOGFILE; exit 1; }
+systemctl enable mariadb                                    >> $LOGFILE || { echo "---Failed to enable MariaDB---" | tee -a $LOGFILE; exit 1; }
 mysql -e "CREATE USER '$USER'@'$HOST' IDENTIFIED BY '$PASSWORD'; GRANT ALL PRIVILEGES ON * . * TO '$USER'@'$HOST'; FLUSH PRIVILEGES;"  >> $LOGFILE 2>&1 || { echo "---Failed to add user---" | tee -a $LOGFILE; exit 1; }
-firewall-cmd --state >> $LOGFILE 2>&1
+firewall-cmd --state >> $LOGFILE
 if [ $? -eq 0 ] ; then
-  firewall-cmd --zone=public --add-port=3306/tcp --permanent  >> $LOGFILE 2>&1 || { echo "---Failed to open port 3306---" | tee -a $LOGFILE; exit 1; }
-  firewall-cmd --reload                                       >> $LOGFILE 2>&1 || { echo "---Failed to reload firewall---" | tee -a $LOGFILE; exit 1; }
+  firewall-cmd --zone=public --add-port=3306/tcp --permanent  >> $LOGFILE || { echo "---Failed to open port 3306---" | tee -a $LOGFILE; exit 1; }
+  firewall-cmd --reload                                       >> $LOGFILE || { echo "---Failed to reload firewall---" | tee -a $LOGFILE; exit 1; }
 fi
 echo "---finish installing mariaDB---" | tee -a $LOGFILE
 echo '{"status":"success"}'
