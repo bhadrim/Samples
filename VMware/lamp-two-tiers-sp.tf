ac9152b8285419c96077f4e99a3448a367b9858b
bhadrim <bhadrim@ca.ibm.com>
2019-05-08 12:18:14 -0400

    Update lamp-two-tiers-sp.tf

--- VMware/lamp-two-tiers-sp.tf
+++ VMware/lamp-two-tiers-sp.tf
@@ -444,7 +444,7 @@ output "Install Maria log"{
 	
 resource "camc_scriptpackage" "install_php" {
   	depends_on = ["camc_scriptpackage.install_mariadb"]	
-  	program = ["cat", ${camc_scriptpackage.install_mariadb.result["loglocation"]}]
+  	program = ["cat", "${camc_scriptpackage.install_mariadb.result["loglocation"]}"]
   	on_create = true
   	remote_host = "${vsphere_virtual_machine.mariadb_vm.clone.0.customize.0.network_interface.0.ipv4_address}"
   	remote_user = "${var.mariadb_ssh_user}"
