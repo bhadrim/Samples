9fe56d7c8304e2d8be5a84fd16b498a736fd276a
bhadrim <bhadrim@ca.ibm.com>
2019-05-08 10:18:00 -0400

    Update lamp-two-tiers-sp.tf

--- VMware/lamp-two-tiers-sp.tf
+++ VMware/lamp-two-tiers-sp.tf
@@ -439,7 +439,7 @@ resource "camc_scriptpackage" "install_mariadb" {
 }
 	
 output "Install Maria DB Script finished with status"{
-  value = "${camc_scriptpackage.install_mariadb.result["status"]}"
+  value = "${camc_scriptpackage.install_mariadb["status"]}"
 }	
 
 resource "camc_scriptpackage" "install_php" {
@@ -456,7 +456,7 @@ resource "camc_scriptpackage" "install_php" {
 }
 	
 output "Install PHP Script finished with status"{
-  value = "${camc_scriptpackage.install_php.result["status"]}"
+  value = "${camc_scriptpackage.install_php["status"]}"
 }		
 
 output "application_url" {
