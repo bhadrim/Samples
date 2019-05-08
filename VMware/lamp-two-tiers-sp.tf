4af374ab9ef0d0028e6c4fc4dc51dcf6dcd72c1e
bhadrim <bhadrim@ca.ibm.com>
2019-05-08 10:22:01 -0400

    Update lamp-two-tiers-sp.tf

--- VMware/lamp-two-tiers-sp.tf
+++ VMware/lamp-two-tiers-sp.tf
@@ -456,7 +456,7 @@ resource "camc_scriptpackage" "install_php" {
 }
 	
 output "Install PHP Script finished with status"{
-  value = "${camc_scriptpackage.install_php.result.status]}"
+  value = "${camc_scriptpackage.install_php.result.status}"
 }		
 
 output "application_url" {
