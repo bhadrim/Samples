3a22424425671edf02a25f6d20b2948ab01696d2
bhadrim <bhadrim@ca.ibm.com>
2020-01-27 14:47:16 -0500

    Create main.tf

---
+++ CAMCUpdate/main.tf
@@ -0,0 +1,24 @@
+resource "camc_scriptpackage" "create" {
+  program = ["/bin/bash","./create.sh"]
+  program_sensitive = ["-p", "${var.password}"]
+  on_create = true
+}
+
+
+resource "camc_scriptpackage" "update" {
+  program = ["/bin/bash","./update.sh"]
+  program_sensitive = ["-p", "${var.password}"]
+  on_update = true
+}
+
+variable "password" {
+  default = "foo"
+}
+
+output "create_result" {
+  value = "${camc_scriptpackage.create.result}"
+}
+
+output "update_result" {
+  value = "${camc_scriptpackage.update.result}"
+}
