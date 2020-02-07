db7ca356fd74bcce6a158b3346cd46456333f585
bhadrim <bhadrim@ca.ibm.com>
2020-02-07 14:34:31 -0500

    Update main.tf

--- CAMCUpdate/main.tf
+++ CAMCUpdate/main.tf
@@ -25,9 +25,9 @@ variable "password" {
 }
 
 output "create_result" {
-  value = "${camc_scriptpackage.create.result}"
+  value = "${camc_updatable_scriptpackage.create.result}"
 }
 
 output "update_result" {
-  value = "${camc_scriptpackage.update.result}"
+  value = "${camc_updatable_scriptpackage.update.result}"
 }
