345e11437aaf22bb49d645e427005d7af5f11052
bhadrim <bhadrim@ca.ibm.com>
2020-02-13 14:18:22 -0500

    Create main.tf

---
+++ TestSubModules/test/unit/ec2/main.tf
@@ -0,0 +1,22 @@
+
+variable "password" {
+  default = "foo"
+}
+
+module "ec2_create"{
+  source="../../../module/ec2"
+  password="${var.password}"
+}
+
+module "ebs_create"{
+  source="../../../module/ebs"
+  password="${var.password}"
+}
+
+output "ec2_out" {
+        value = "${module.ec2_create.create_result}"
+}
+
+output "ebs_out" {
+        value = "${module.ebs_create.create_result}"
+}
