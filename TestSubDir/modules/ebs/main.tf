90c6f03ceaacefd45db610ce9dfc9866fb7409d4
bhadrim <bhadrim@ca.ibm.com>
2020-02-13 12:00:46 -0500

    Update main.tf

--- TestSubDir/modules/ebs/main.tf
+++ TestSubDir/modules/ebs/main.tf
@@ -3,12 +3,12 @@ variable "password" {
   default = "foo"
 }
 
-resource "camc_scriptpackage" "create" {
+resource "camc_scriptpackage" "ec2_create" {
   program = ["/bin/bash","./create.sh"]
   program_sensitive = ["-p", "${var.password}"]
   on_create = true
 }
 
 output "create_result" {
-  value = "${camc_scriptpackage.create.result}"
+  value = "${camc_scriptpackage.ec2_create.result}"
 }
